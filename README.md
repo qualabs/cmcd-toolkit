fb229a17d24c9557ca0fc22f902ac2e75f912be9
Nicolas Levy <nicolas@qualabs.com>
2025-05-09 16:19:51 -0300

    add license, basic readme, reorder folder, remove collector pubsub schema

--- README.md
+++ README.md
@@ -1,10 +1,38 @@
-# cmcdv2-prometheus
+# cmcd-toolkit
 
-Run `docker compose up`
+cmcd-toolkit is a set of basic tools to collect and analyze data sent by video with [CMCD v2](https://github.com/cta-wave/common-media-client-data) support.
 
-Player will be available at: http://localhost:8080
-Colloctor will run at: http://localhost:3000
+This repository provides tools to collect and analize CMCD v2 locally and in different cloud providers.
+
+## How to deploy in Google Cloud
+
+Deployment in Google Cloud:
+![CMCD Toolkit Google Cloud Deployment Example](docs/gcloud-example.png)
+
+Assuming you already have a Google Cloud account, the high-level steps to deploy are:
+1. Create a CMCD [BigTable](https://cloud.google.com/bigtable) using schema found in the `gcloud-big-table` folder
+2. Create a [Pub/Sub](https://cloud.google.com/pubsub) topic and a suscription to the CMCD BigTable
+3. Create a [Cloud Run Function](https://cloud.google.com/functions) with the code found in `index.js` from `collector-gcloud-function`. This will give you a `{public url}` for the collector.
+4. Configure in the palyer found in the `palyer` folder the following urls:
+    * For response mode: `{public url}/cmcd/response-mode`
+    * For event mode: `{public url}/cmcd/event-mode`
+5. (Optional) Create a bucket in [Cloud Storage](https://cloud.google.com/storage) with public access and deploy the palyer for testing the system.
+6. (Optional) Create a bucket in [Cloud Storage](https://cloud.google.com/storage) and suscribe to the Pub/Sub CMCD topic for long term CMCD storage.
+7. (Optinal) Connect a Grafana instance using the BigQuery plugin. Find a Grafana config example in the `grafana` folder.
+
+## How to run localy
+
+*Notice: This is work in progres.*
+
+1. Run `docker compose up`
+
+Player will be available at: [http://localhost:8080](http://localhost:8080)
+Colloctor will run at: [http://localhost:3000](http://localhost:3000)
 
 ## How to develop
 
 Copy `docker-comose.develop.yml` to `docker-compose.override.yml` and then run `docker compose up`. You will be able to modify the code while running the project
+
+## License
+
+This project is licensed under the Apache 2.0 License. See the `LICENCE` file for more details.
--- collector/src/pubsub.js
+++ collector/src/pubsub.js
@@ -1,75 +1,19 @@
 import fs from 'fs';
 import { PubSub } from '@google-cloud/pubsub';
-import {PUBSUB_ENABLED, PUBSUB_TOPIC_NAME, PUBSUB_USE_SCHEMA } from './utils/config.js';
+import {PUBSUB_ENABLED, PUBSUB_TOPIC_NAME } from './utils/config.js';
 
-const schemaRaw = fs.readFileSync('./pub-sub-schema.json', 'utf8');
-const pubSubSchema = JSON.parse(schemaRaw);
 const pubsub = new PubSub();
 
 const saveData = async (body) => {
     if (PUBSUB_ENABLED){
         try {
-            const pubsubMessage = PUBSUB_USE_SCHEMA ? transformDataForPubSub(body, pubSubSchema) : body 
-            const messageBuffer = Buffer.from(JSON.stringify(pubsubMessage));
+            const messageBuffer = Buffer.from(JSON.stringify(body));
             const messageId = await pubsub.topic(PUBSUB_TOPIC_NAME).publishMessage({data: messageBuffer});
             console.log(`Message ${messageId} published to ${PUBSUB_TOPIC_NAME}.`);
         } catch (error) {
             console.error(`Received error while publishing: ${error.message}`);
         }
     }
-}
-
-/**
- * Transforms a CMCD data object according to the Pub/Sub Avro schema.
- * - Adds missing fields from the schema with a null value.
- * - Wraps existing values in an object with their primary Avro type.
- *
- * @param {object} inputObject The input object with CMCD data.
- * @param {object} schema The Pub/Sub Avro schema object.
- * @returns {object} The transformed object.
- */
-function transformDataForPubSub(inputObject, schema) {
-    if (!inputObject || typeof inputObject !== 'object') {
-        console.error("Input must be a valid object.");
-        return {};
-    }
-    if (!schema || !Array.isArray(schema.fields)) {
-        console.error("Invalid schema or it does not contain 'fields'.");
-        return {};
-    }
-
-    const outputObject = {};
-    const schemaFields = schema.fields;
-
-    // Iterate over each field defined in the schema
-    for (const field of schemaFields) {
-        const fieldName = field.name;
-        const fieldTypes = field.type; // It's an array like ["null", "string"]
-
-        // Check if the key exists in the input object
-        if (inputObject.hasOwnProperty(fieldName)) {
-            const inputValue = inputObject[fieldName];
-
-            // Find the primary type (not 'null') from the types array
-            const primaryType = fieldTypes.find(t => t !== 'null');
-
-            if (primaryType) {
-                // Create the object { "type": value }
-                outputObject[fieldName] = { [primaryType]: inputValue };
-            } else {
-                // Unlikely case if the schema doesn't follow the ["null", type] pattern
-                console.warn(`Could not determine the primary type for field: ${fieldName}`);
-                // You could assign null or handle it differently
-                outputObject[fieldName] = null;
-            }
-        } else {
-            // If the schema key is not in the input object, add it with a null value
-            outputObject[fieldName] = null;
-        }
-    }
-
-    // Note: Keys in inputObject that are NOT in schema.fields will be ignored.
-    return outputObject;
 }
 
 export default saveData; 
